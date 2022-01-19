58fd8d4e3e56f45106cc1d04cb4ecef0218d2e2f
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-19 17:36:40 +0000

    feat: Added Avatar and Updated UI - Added the Avatar prop to the Card component - Updated the UI and styled also

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 import "./index.css";
 import { Zap, Clock } from "react-feather";
 
-import { Card } from "./components/Card/Card";
+import { Card, Avatar } from "./components/Card/Card";
 
 function App() {
   return (
     <div className="bg-neutral-main-dark-blue">
       <div className="max-w-screen-mobile mx-auto font-sans py-16">
-        <Card className="bg-neutral-card-dark-blue mx-auto px-8 rounded-2xl">
-          <div className="py-8 overflow-hidden">
-            <img src="/equilibrium.jpg" alt="NFT Logo" className="rounded-2xl" />
+        <Card className="bg-neutral-card-dark-blue mx-auto p-8 rounded-2xl">
+          <div className="overflow-hidden">
+            <img src="/equilibrium.jpg" alt="Equilibrium Logo" className="rounded-2xl" />
           </div>
-          <div className="space-y-2">
-            <h3 className="text-white text-lg font-bold">Equilibrium #3429</h3>
-            <p className="text-white text-sm font-light opacity-50">
+          <div className="mt-6 space-y-3">
+            <h3 className="text-white text-2xl font-bold">Equilibrium #3429</h3>
+            <p className="text-primary-soft-blue text-lg font-light">
               Our Equilibrium collection promotes balance and calm
             </p>
-            <div className="flex justify-between">
-              <div className="flex text-primary-cyan space-x-1">
-                <Zap className="fill-primary-cyan" />
-                <span>0.041 ETH</span>
+            <div className="flex justify-between py-2">
+              <div className="flex text-primary-cyan items-center space-x-1">
+                <Zap className="fill-primary-cyan w-4 h-4" />
+                <span className="text-sm">0.041 ETH</span>
               </div>
-              <div className="flex text-white opacity-50 space-x-1">
-                <Clock className="" />
-                <span>3 days left</span>
+              <div className="flex text-primary-soft-blue items-center space-x-1">
+                <Clock className="w-4 h-4" />
+                <span className="text-sm">3 days left</span>
               </div>
-              <hr className="bg-neutral-line-dark-blue" />
             </div>
+          </div>
+          <hr className="h-px my-4 text-neutral-line-dark-blue" />
+          <div className="flex items-center space-x-4">
+            <Avatar src="/avatar.png" alt="Avatar" className="border border-white rounded-full w-8 h-8" />
+            <p className="text-primary-soft-blue">
+              Created by <span className="text-white font-semibold">Hayden Buckley-Smith</span>
+            </p>
           </div>
         </Card>
       </div>
