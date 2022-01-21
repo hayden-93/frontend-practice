aafce9c8504f4623981be022dd4f2f754dbfce19
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-21 11:35:32 +0000

    refactor: Card Data - Created mock data as nftPreviews initial state - Updated Card Component to use new data properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import { faker } from "./utils";
 import {
   Avatar,
   Card,
@@ -15,32 +16,53 @@ import {
   Layout,
 } from "./components";
 
+const createMocks = (() => [
+  {
+    id: faker.uuid(),
+    image: { src: "/equilibrium.jpg", alt: "Equilibrium Logo" },
+    title: "Equilibrium #3429",
+    description: "Our Equilibrium collection promotes balance and calm.",
+    price: 0.041,
+    deadline: { time: 3, format: "day" },
+    user: { name: "Jules Wyvern", avatar: { src: "/avatar.png", alt: "Avatar" } },
+  },
+  ...new Array(3).fill(null).map(() => ({
+    id: faker.uuid(),
+    image: { src: faker.image(), alt: faker.description() },
+    title: `${faker.name()} #${faker.number()}`,
+    description: faker.description(),
+    price: faker.number({ min: 0, max: 1, precision: 0.001 }),
+    deadline: {
+      time: faker.number({ min: 1, max: 30, precision: 1 }),
+      format: faker.random(["second", "minute", "hour", "day", "week"]),
+    },
+    user: { name: faker.fullName(), avatar: { src: faker.avatar(), alt: faker.description() } },
+  })),
+])();
+
 function App() {
-  const [image] = useState({ src: "/equilibrium.jpg", alt: "Equilibrium Logo" });
-  const [title] = useState("Equilibrium #3429");
-  const [description] = useState("Our Equilibrium collection promotes balance and calm");
-  const [price] = useState("0.041 ETH");
-  const [deadline] = useState("3 days left");
-  const [name] = useState("Hayden Buckley-Smith");
+  const [nftPreviews, setNftPreviews] = useState(() => createMocks);
 
   return (
     <Layout>
-      <Card>
-        <CardImage src={image.src} alt={image.alt} />
-        <CardContent className="mt-6">
-          <CardTitle>{title}</CardTitle>
-          <CardDescription>{description}</CardDescription>
-          <div className="flex justify-between py-2">
-            <CardPrice>{price}</CardPrice>
-            <CardDeadline>{deadline}</CardDeadline>
-          </div>
-        </CardContent>
-        <CardSeperator />
-        <CardProfile>
-          <Avatar src="/avatar.png" alt="Avatar" />
-          <CardProfileName>{name}</CardProfileName>
-        </CardProfile>
-      </Card>
+      {nftPreviews.map(({ id, image, title, description, price, deadline, user }) => (
+        <Card key={id}>
+          <CardImage src={image.src} alt={image.alt} />
+          <CardContent className="mt-6">
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+            <div className="flex justify-between py-2">
+              <CardPrice>{price}</CardPrice>
+              <CardDeadline timeRemaining={deadline.time} format={deadline.format} />
+            </div>
+          </CardContent>
+          <CardSeperator />
+          <CardProfile>
+            <Avatar src={user.avatar.src} alt={user.avatar.alt} />
+            <CardProfileName>{user.name}</CardProfileName>
+          </CardProfile>
+        </Card>
+      ))}
     </Layout>
   );
 }
