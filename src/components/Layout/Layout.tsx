376381e00527c78be43724a5bc59602c6e4f7eff
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-21 11:30:45 +0000

    refactor: Layout Component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,10 +6,10 @@ interface LayoutProps {
 }
 
 export const Layout = ({ children, className }: LayoutProps) => {
-  const classes = ["bg-neutral-main-dark-blue h-screen", className].join(" ").trim();
+  const classes = ["bg-neutral-main-dark-blue min-h-screen", className].join(" ").trim();
   return (
     <div className={classes}>
-      <div className="max-w-screen-mobile mx-auto font-sans py-16">{children}</div>
+      <div className="max-w-screen-mobile mx-auto font-sans py-16 space-y-8">{children}</div>
     </div>
   );
 };
