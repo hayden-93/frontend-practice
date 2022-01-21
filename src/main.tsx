6fe34a1525c5cdf65621fb3b31e349ebd8474d3a
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-21 13:47:15 +0000

    Setup msw - Initiliased service worker in /public - Setup worker and added worker handlers - Added logic to start worker only during development in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import "./index.css";
 
 import App from "./App";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,33 @@
+import { rest } from "msw";
+
+import { faker } from "../utils";
+
+const mockData = [
+  {
+    id: faker.uuid(),
+    image: { src: "/equilibrium.jpg", alt: "Equilibrium Logo" },
+    title: "Equilibrium #3429",
+    description: "Our Equilibrium collection promotes balance and calm.",
+    price: 0.041,
+    deadline: { time: 3, format: "day" },
+    user: { name: "Jules Wyvern", avatar: { src: "/avatar.png", alt: "Avatar" } },
+  },
+  ...new Array(3).fill(null).map(() => ({
+    id: faker.uuid(),
+    image: { src: faker.image(), alt: faker.description() },
+    title: `${faker.name()} #${faker.number()}`,
+    description: faker.description(),
+    price: faker.number({ min: 0, max: 1, precision: 0.001 }),
+    deadline: {
+      time: faker.number({ min: 1, max: 30, precision: 1 }),
+      format: faker.random(["second", "minute", "hour", "day", "week"]),
+    },
+    user: { name: faker.fullName(), avatar: { src: faker.avatar(), alt: faker.description() } },
+  })),
+];
+
+export const handlers = [
+  rest.get("/marketplace", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockData));
+  }),
+];
