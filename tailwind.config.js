0e42d6b7b255ac90dbfb3a566d7dc34f9df5540b
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-03 14:41:30 +0000

    feat: Updated custom colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
       "neutral-main-dark-blue": "#0d192b",
       "neutral-card-dark-blue": "#14253d",
       "neutral-line-dark-blue": "#2f415b",
+      "neutral-light-gray": "hsl(0, 0%, 81%)",
+      "neutral-light-grayish-blue": "hsl(210, 46%, 95%)",
       white: "#ffffff",
+      violet: "hsl(263, 55%, 52%)",
+      "dark-grayish-blue": "hsl(217, 19%, 35%)",
+      "dark-blackish-blue": "hsl(219, 29%, 14%)",
     },
     screens: {
       mobile: "375px",
