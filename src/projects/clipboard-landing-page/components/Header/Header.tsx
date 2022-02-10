e207762fa58941ff629d05e9e75099db57b9ef8d
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-10 15:08:12 +0000

    feat: Header Component - Created Header component with the sub component of HeaderLogo

---
+++ src/projects/clipboard-landing-page/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from "react";
+
+interface HeaderProps {
+  className?: string;
+}
+
+export const Header = ({ children, className, ...props }: PropsWithChildren<HeaderProps>) => {
+  const classes = ["", className].join(" ").trim();
+  return (
+    <header className={classes}>
+      <img src="../../clipboard-landing-page/bg-header-desktop.png" alt="Desktop Header Background" />
+      {children}
+    </header>
+  );
+};
+
+interface HeaderLogoProps {
+  className?: string;
+  src: string;
+  alt: string;
+}
+
+const HeaderLogo = ({ className, ...props }: HeaderLogoProps) => {
+  const classes = ["", className].join(" ").trim();
+  return <img className={classes} {...props} />;
+};
+
+Header.Logo = HeaderLogo;
