d3a14cbf2831cde885a903dc6f90f7697d401472
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-15 14:56:00 +0000

    refactor: Refactored the Header Component

--- src/projects/sunnyside-agency/components/Header/Header.tsx
+++ src/projects/sunnyside-agency/components/Header/Header.tsx
@@ -3,15 +3,14 @@ interface HeaderProps {
 }
 
 export const Header = ({ className }: HeaderProps) => {
-  const classes = ["max-w-screen-desktop mx-auto h-screen", className].join(" ").trim();
+  const classes = ["max-w-screen-desktop mx-auto", className].join(" ").trim();
   return (
-    <header className="">
-      <div className="relative">
-        <img src="/sunnyside-agency/image-header.jpg" alt="Rainbow Image" className="h-screen w-full absolute" />
-        <img src="/sunnyside-agency/logo.svg" alt="Sunnyside Agency Logo" className="absolute py-12 px-6" />
+    <>
+      <header className={classes}>
+        <img src="/sunnyside-agency/logo.svg" alt="Sunnyside Agency Logo" className="absolute py-8 px-6 w-52 h-auto" />
         <div className="flex justify-end">
           <nav className="absolute">
-            <ul className="flex justify-end">
+            <ul className="flex justify-end space-x-12 items-center mt-5 py-3 px-10 text-lg">
               <li className="text-white">
                 <a href="#">About</a>
               </li>
@@ -21,17 +20,19 @@ export const Header = ({ className }: HeaderProps) => {
               <li className="text-white">
                 <a href="#">Projects</a>
               </li>
-              <button className="rounded-full bg-white py-3 px-4 font-medium">CONTACT</button>
+              <button className="rounded-full bg-white py-3 px-6 font-medium">CONTACT</button>
             </ul>
           </nav>
         </div>
-        <div className="justify-center">
-          <h1 className="absolute text-white uppercase">We Are Creatives</h1>
+        <div className="max-w-screen-sm mx-auto">
+          <h1 className="absolute text-white text-6xl font-extrabold tracking-widest uppercase top-36 ml-12">
+            We Are Creatives
+          </h1>
         </div>
-        <div className="text-center">
-          <img src="/sunnyside-agency/icon-arrow-down.svg" alt="Down Arrow Icon" className="absolute" />
+        <div className="relative max-w-screen-sm mx-auto left-80 place-items-center">
+          <img src="/sunnyside-agency/icon-arrow-down.svg" alt="Down Arrow Icon" className="absolute top-72" />
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
