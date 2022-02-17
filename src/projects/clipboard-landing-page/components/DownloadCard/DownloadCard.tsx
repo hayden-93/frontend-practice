deaff218dddf52bc7cb1872b05e5c3951a91b5aa
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-17 09:23:58 +0000

    refactor: DownloadCard Component - Removed the Button from this component and created a seperate component as it is used throughout the app - Change base styles for the Title and Description to enlarge the font size - Enlarged the max screen width to the next higher option

--- src/projects/clipboard-landing-page/components/DownloadCard/DownloadCard.tsx
+++ src/projects/clipboard-landing-page/components/DownloadCard/DownloadCard.tsx
@@ -5,7 +5,7 @@ interface DownloadCardProps {
 }
 
 export const DownloadCard = ({ children, className }: PropsWithChildren<DownloadCardProps>) => {
-  const classes = ["max-w-screen-xs mx-auto text-center space-y-4", className].join(" ").trim();
+  const classes = ["max-w-screen-sm mx-auto text-center space-y-4", className].join(" ").trim();
 
   return <div className={classes}>{children}</div>;
 };
@@ -14,40 +14,21 @@ interface DownloadCardTitleProps {
   className?: string;
 }
 
-const DownloadCardTitle = ({ className }: DownloadCardTitleProps) => {
-  const classes = ["text-3xl text-clipboard-dark-grayish-blue", className].join(" ").trim();
+const DownloadCardTitle = ({ children, className }: PropsWithChildren<DownloadCardTitleProps>) => {
+  const classes = ["text-5xl font-semibold text-clipboard-dark-grayish-blue", className].join(" ").trim();
 
-  return <h2 className={classes}>A history of everything you copy</h2>;
+  return <h2 className={classes}>{children}</h2>;
 };
 
 interface DownloadCardDescriptionProps {
   className?: string;
 }
 
-const DownloadCardDescription = ({ className }: DownloadCardDescriptionProps) => {
-  const classes = ["text-sm text-clipboard-grayish-blue", className].join(" ").trim();
+const DownloadCardDescription = ({ children, className }: PropsWithChildren<DownloadCardDescriptionProps>) => {
+  const classes = ["text-lg text-clipboard-grayish-blue", className].join(" ").trim();
 
-  return (
-    <p className={classes}>
-      Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your
-      devices.
-    </p>
-  );
-};
-interface DownloadCardButtonProps {
-  className?: string;
-}
-
-const DownloadCardButton = ({ children, className }: PropsWithChildren<DownloadCardButtonProps>) => {
-  const classes = ["text-white py-2 px-6 text-sm rounded-full shadow border-b", className].join(" ").trim();
-
-  return (
-    <>
-      <button className={classes}>{children}</button>
-    </>
-  );
+  return <p className={classes}>{children}</p>;
 };
 
 DownloadCard.Title = DownloadCardTitle;
 DownloadCard.Description = DownloadCardDescription;
-DownloadCard.Button = DownloadCardButton;
