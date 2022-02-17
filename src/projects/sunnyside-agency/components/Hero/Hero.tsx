38b1b7bb27a5c5531f4041c48582ca5bf4b01ba1
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-17 15:44:51 +0000

    chore: Created sub-components to introduce onto my Hero section (HeroTitle, HeroArrow)

--- src/projects/sunnyside-agency/components/Hero/Hero.tsx
+++ src/projects/sunnyside-agency/components/Hero/Hero.tsx
@@ -1,13 +1,41 @@
+import { PropsWithChildren } from "react";
+
 interface HeroProps {
   className?: string;
 }
 
-export const Hero = ({ className }: HeroProps) => {
+export const Hero = ({ children, className }: PropsWithChildren<HeroProps>) => {
   const classes = ["bg-sunnyside bg-cover bg-center bg-no-repeat h-screen", className].join(" ").trim();
 
+  return <section className={classes}>{children}</section>;
+};
+
+interface HeroTitleProps {
+  className?: string;
+}
+
+const HeroTitle = ({ children, className }: PropsWithChildren<HeroTitleProps>) => {
+  const classes = ["max-w-screen-sm mx-auto pt-48 text-center text-white text-6xl uppercase", className]
+    .join(" ")
+    .trim();
+
+  return <h1 className={classes}>{children}</h1>;
+};
+interface HeroArrowProps {
+  className?: string;
+}
+
+const HeroArrow = ({ children, className }: PropsWithChildren<HeroArrowProps>) => {
+  const classes = ["max-w-screen-xxs mx-auto mt-24 text-center text-white", className].join(" ").trim();
+
   return (
-    <section className={classes}>
-      <div className=""></div>
-    </section>
+    <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg" className={classes}>
+      <g stroke="#FFF" stroke-width="6" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M18 3v100M3 95.484l15 15 15-15" />
+      </g>
+    </svg>
   );
 };
+
+Hero.Title = HeroTitle;
+Hero.Arrow = HeroArrow;
