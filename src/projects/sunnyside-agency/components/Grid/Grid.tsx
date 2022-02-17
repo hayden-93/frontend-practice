b0a711c4311861ff6de38873734ddd6629c73726
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-17 13:25:44 +0000

    Avatar & Grid Components - Created a Grid component for use of images section - Created an Avatar component for resuability as it is used on multiple occassions - Imported Avatar and Grid into index.tsx

---
+++ src/projects/sunnyside-agency/components/Grid/Grid.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren } from "react";
+
+interface GridProps {
+  className?: string;
+}
+
+export const Grid = ({ children, className, ...props }: PropsWithChildren<GridProps>) => {
+  const classes = ["flex max-w-screen-desktop", className].join(" ").trim();
+
+  return (
+    <section className={classes} {...props}>
+      {children}
+    </section>
+  );
+};
+
+interface GridImageProps {
+  className?: string;
+  src: string;
+  alt: string;
+}
+
+const GridImage = ({ className, ...props }: GridImageProps) => {
+  const classes = ["", className].join(" ").trim();
+
+  return <img className={classes} {...props} />;
+};
+
+Grid.Image = GridImage;
--- src/projects/sunnyside-agency/components/index.tsx
+++ src/projects/sunnyside-agency/components/index.tsx
@@ -3,3 +3,5 @@ export * from "./Header";
 export * from "./Hero";
 export * from "./Card";
 export * from "./Testimonials";
+export * from "./Avatar";
+export * from "./Grid";
