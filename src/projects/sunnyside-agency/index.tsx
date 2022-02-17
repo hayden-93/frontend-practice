ee47307f63f675ea01dc79284f9021225f0a9fb0
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-17 15:47:53 +0000

    refactor: Index changes - Imported the new components (Avatar, Footer, Grid) and implemented them within the app in the correct layout

--- src/projects/sunnyside-agency/index.tsx
+++ src/projects/sunnyside-agency/index.tsx
@@ -1,11 +1,14 @@
-import { Card, Header, Hero, Testimonials } from "./components";
+import { Avatar, Card, Footer, Grid, Header, Hero, Testimonials } from "./components";
 
 export function SunnysideAgency() {
   return (
-    <>
+    <div>
       <Header />
-      <Hero />
-      {/* <section className="grid grid-cols-2 text-left">
+      <Hero>
+        <Hero.Title>We Are Creatives</Hero.Title>
+        <Hero.Arrow />
+      </Hero>
+      <section className="grid grid-cols-2 text-left">
         <div className="max-w-screen-sm mx-auto self-center space-y-4">
           <h3 className="text-4xl font-black w-80">Transform your brand</h3>
           <p className="text-sunny-dark-grayish-blue text-base font-extrabold w-96">
@@ -28,55 +31,72 @@ export function SunnysideAgency() {
           </p>
           <button className="py-4 uppercase font-black underline decoration-yellow decoration-4">Learn More</button>
         </div>
-        <div className="relative text-center space-y-3">
-          <img src="/sunnyside-agency/image-graphic-design.jpg" alt="Cherry Image" className="absolute" />
+        <div className="text-center space-y-3">
+          <img src="/sunnyside-agency/image-graphic-design.jpg" alt="Cherry Image" className="" />
           <h3 className="relative">Graphic Design</h3>
           <p className="relative max-w-screen-xxs mx-auto">
             Great design makes you memorable. We deliver artwork that underscores your brand message and captures
             potential clients' attention.
           </p>
         </div>
-        <div className="relative text-center">
-          <img src="/sunnyside-agency/image-photography.jpg" alt="Orange Image" className="absolute" />
+        <div className="text-center">
+          <img src="/sunnyside-agency/image-photography.jpg" alt="Orange Image" className="" />
           <h3 className="relative">Photography</h3>
           <p className="relative">
             Increase your credibility by getting the most stunning, high-quality photos that improve your business
             image.
           </p>
         </div>
-      </section> */}
-
-      <Testimonials>
-        <h1>Client Testimonials</h1>
-        <div className="flex">
-          <Card>
-            <Card.Avatar src="/sunnyside-agency/image-emily.jpg" alt="Emily" className="" />
+      </section>
+      <Testimonials className="mt-20">
+        <h1 className="text-center text-2xl text-sunny-grayish-blue font-black tracking-widest uppercase">
+          Client Testimonials
+        </h1>
+        <div className="flex mt-10">
+          <Card className="items-center">
+            <Avatar src="/sunnyside-agency/image-emily.jpg" alt="Emily" className="" />
             <Card.Description>
               We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always
               hit.
             </Card.Description>
-            <Card.Author>Emily R.</Card.Author>
-            <Card.JobTitle>Marketing Director</Card.JobTitle>
+            <div>
+              <Card.Author>Emily R.</Card.Author>
+              <Card.JobTitle>Marketing Director</Card.JobTitle>
+            </div>
           </Card>
-          <Card>
-            <Card.Avatar src="/sunnyside-agency/image-emily.jpg" alt="Emily" className="" />
+          <Card className="">
+            <Avatar src="/sunnyside-agency/image-thomas.jpg" alt="Thomas" className="" />
             <Card.Description>
               Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and
               enjoyable experience.
             </Card.Description>
-            <Card.Author>Thomas S.</Card.Author>
-            <Card.JobTitle>Chief Operating Officer</Card.JobTitle>
+            <div>
+              <Card.Author>Thomas S.</Card.Author>
+              <Card.JobTitle>Chief Operating Officer</Card.JobTitle>
+            </div>
           </Card>
-          <Card>
-            <Card.Avatar src="/sunnyside-agency/image-emily.jpg" alt="Emily" className="" />
+          <Card className="">
+            <Avatar src="/sunnyside-agency/image-jennie.jpg" alt="Jennie" className="" />
             <Card.Description>
               Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
             </Card.Description>
-            <Card.Author>Jennie F.</Card.Author>
-            <Card.JobTitle>Business Owner</Card.JobTitle>
+            <div>
+              <Card.Author>Jennie F.</Card.Author>
+              <Card.JobTitle>Business Owner</Card.JobTitle>
+            </div>
           </Card>
         </div>
       </Testimonials>
-    </>
+      <Grid>
+        <Grid.Image src="/sunnyside-agency/image-gallery-milkbottles.jpg" alt="Milk Bottles" />
+        <Grid.Image src="/sunnyside-agency/image-gallery-orange.jpg" alt="Orange" />
+        <Grid.Image src="/sunnyside-agency/image-gallery-cone.jpg" alt="Ice Cream Cone" />
+        <Grid.Image src="/sunnyside-agency/image-gallery-sugarcubes.jpg" alt="Sugar Cubes Stacked" />
+      </Grid>
+      <Footer>
+        <Footer.NavLinks></Footer.NavLinks>
+        <Footer.Social></Footer.Social>
+      </Footer>
+    </div>
   );
 }
