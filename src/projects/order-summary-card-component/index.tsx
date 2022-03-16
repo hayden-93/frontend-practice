33a1b1a9cc17e4d5b34f6482e223c8dddbc0f65e
Hayden Buckley-Smith <hayden@abstack.co>
2022-03-16 07:55:23 +0000

    chore: Updated the index.tsx with basic Compnents ready for styling

--- src/projects/order-summary-card-component/index.tsx
+++ src/projects/order-summary-card-component/index.tsx
@@ -1,7 +1,27 @@
+import { Button } from "./components";
+import { Card } from "./components/Card";
+import { Layout } from "./components/Layout/Layout";
+
 export function OrderSummary() {
   return (
-    <>
-      <></>
-    </>
+    <Layout>
+      <Card>
+        <Card.Image
+          src="/order-summary-card-component/illustration-hero.svg"
+          alt="Woman Dancing With Headphones Image"
+        />
+        <Card.Title>Order Summary</Card.Title>
+        <Card.Description>
+          You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!
+        </Card.Description>
+        <div></div>
+        <div>
+          <Button>Proceed to Payment</Button>
+        </div>
+        <div>
+          <Button>Cancel Order</Button>
+        </div>
+      </Card>
+    </Layout>
   );
 }
