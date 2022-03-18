63b23247ff398bb4f35f9eb009be2511be2fb5ec
Hayden Buckley-Smith <hayden@abstack.co>
2022-03-18 09:23:18 +0000

    feat: Created new function ArticlePreview to show on projects

---
+++ src/projects/article-preview/index.tsx
@@ -0,0 +1,9 @@
+import { Avatar } from "./components";
+
+export function ArticlePreview() {
+  return (
+    <section>
+      <Avatar src="/article-preview/avatar-michelle.jpg" alt="Photo of Michelle" />
+    </section>
+  );
+}
